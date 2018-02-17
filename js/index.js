29bcb90c9f78e659fc717e58b301694891693066
Branden Ng <brandendanielng@gmail.com>
2018-02-17 21:17:07 +1100

    stytle changes

--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@ searchForm.addEventListener('submit', e => {
 
 
 function search(searchTerm, searchLimit, sortBy) {
-  return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
+  return fetch(`https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
   .then(res => res.json())
   .then(data => data.data.children.map(data => data.data))
   .catch(err => console.log(err));
